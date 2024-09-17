8dd8248e141c6996c2ca49316432bd07e9b09b67
George Owen <gowen@esri.com>
2024-09-17 14:24:34 -0500

    revert for preview review

--- src/EsriLeafletStaticBasemapTile.js
+++
@@ -1,6 +0,0 @@
-import packageInfo from '../package.json';
-const version = packageInfo.version;
-export { version as VERSION };
-
-export { StaticBasemapTileLayer, staticBasemapTileLayer } from './StaticBasemapTileLayer';
-export { getSelf } from './Util';
